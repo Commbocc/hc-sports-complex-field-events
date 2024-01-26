17989d065c05db2d657e940cfcaa0139dd55b90b
Jordan Pisani <pisanij@hillsboroughcounty.org>
2024-01-26 11:23:15 -0500

    remove heroku caching proxy

--- plugins/airtable.ts
+++ plugins/airtable.ts
@@ -2,15 +2,20 @@ import axios, { AxiosInstance } from "axios";
 
 const TABLE_ID = `appNFrINNNMTc8GCn`;
 
+// const airtable: AxiosInstance = axios.create({
+//   baseURL: import.meta.env.PROD
+//     ? `https://hc-caching-proxy.herokuapp.com/airtable/${TABLE_ID}`
+//     : `https://api.airtable.com/v0/${TABLE_ID}`,
+//   headers: import.meta.env.DEV
+//     ? { Authorization: `Bearer key1XrFuYhG1GKHvu` }
+//     : undefined,
+// })
+
 const airtable: AxiosInstance = axios.create({
-  baseURL: import.meta.env.PROD
-    ? `https://hc-caching-proxy.herokuapp.com/airtable/${TABLE_ID}`
-    : `https://api.airtable.com/v0/${TABLE_ID}`,
-  headers: import.meta.env.DEV
-    ? {
-        Authorization: `Bearer patCMqNApPrJE8SXi.143baa8db7f29e133b886a0c24776496d00671e7ac993231593b78a2cfe599a6`,
-      }
-    : undefined,
+  baseURL: `https://api.airtable.com/v0/${TABLE_ID}`,
+  headers: {
+    Authorization: `Bearer patCMqNApPrJE8SXi.143baa8db7f29e133b886a0c24776496d00671e7ac993231593b78a2cfe599a6`,
+  },
 });
 
 export default defineNuxtPlugin(() => {
