9d00d7a0a6e6207f29c74551f63ba956e455b7c1
Jordan Pisani <pisanij@hillsboroughcounty.org>
2024-07-10 08:52:52 -0400

    force list view, add padding to top

--- pages/[locationUid].vue
+++ pages/[locationUid].vue
@@ -1,7 +1,7 @@
 <script setup lang="ts">
-const { events, groupedEvents, fetchEvents } = useEvents()
-const { thisWeek, formatIso } = useCalendar()
-await fetchEvents()
+const { events, groupedEvents, fetchEvents } = useEvents();
+const { thisWeek, formatIso } = useCalendar();
+await fetchEvents();
 </script>
 
 <template>
@@ -9,10 +9,9 @@ await fetchEvents()
 
   <h3 v-else-if="!events.records.length">No events at this time</h3>
 
-  <div v-else class="table-responsive">
+  <div v-else class="table-responsive pt-3">
     <table class="table table-sm table-bordered table-striped">
       <thead>
-        <th scope="col"></th>
         <th scope="col" v-for="day in thisWeek">
           {{ formatIso(day) }}
         </th>
@@ -21,7 +20,7 @@ await fetchEvents()
         <tr v-for="(events, field) of groupedEvents">
           <th scope="row">{{ field }}</th>
           <td v-for="day in thisWeek" :data-cell="formatIso(day)">
-            {{ events.find((e) => e.date === day)?.name ?? 'All Day Rented' }}
+            {{ events.find((e) => e.date === day)?.name ?? "All Day Rented" }}
           </td>
         </tr>
       </tbody>
@@ -30,18 +29,16 @@ await fetchEvents()
 </template>
 
 <style>
-@media (max-width: 725px) {
-  thead th {
-    display: none;
-  }
+thead th {
+  display: none;
+}
 
-  td {
-    display: block;
-  }
+td {
+  display: block;
+}
 
-  td::before {
-    content: attr(data-cell) ': ';
-    font-weight: 700;
-  }
+td::before {
+  content: attr(data-cell) ": ";
+  font-weight: 700;
 }
 </style>
